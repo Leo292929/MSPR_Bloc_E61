9b9dcd78abd94d849b9ae43543aebc0ba690b7be
Leo292929 <leo.fougeroud@ecoles-epsi.net>
2024-01-31 11:27:05 +0100

    modif du gitignore pour ajouter le test

---
+++ test/test_pytest.py
@@ -0,0 +1,13 @@
+import dataAug
+import dataCleanning
+import dataLoading
+
+def test_augData():
+    assert 1==1
+
+def test_cleanData():
+    assert 1==1
+
+def test_loadData():
+    assert 1==1
+
